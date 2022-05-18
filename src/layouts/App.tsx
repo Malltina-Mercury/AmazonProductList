95af411f2e3848cb888c83239eb0e23675e849c7
Masud Afsar <masud@afsar.info>
2022-05-18 11:26:37 +0430

    Implement search view component

--- src/layouts/App.tsx
+++ src/layouts/App.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import {SafeAreaView, StatusBar, useColorScheme, View} from 'react-native';
 import tw from 'twrnc';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {SearchView} from '../views/SearchView';
+
+const Stack = createNativeStackNavigator();
 
 interface Props {}
 
-export const App: React.FC<Props> = ({children}) => {
+export const App: React.FC<Props> = () => {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <SafeAreaView>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <View style={tw`bg-gray-100 h-full`}>{children}</View>
+      <View style={tw`bg-gray-100 h-full`}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name={'search'} component={SearchView} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </View>
     </SafeAreaView>
   );
 };
---
+++ src/views/SearchView.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+interface Props {}
+
+export const SearchView: React.FC<Props> = () => {
+  return (
+    <View>
+      <View>
+        <Text>Search </Text>
+      </View>
+    </View>
+  );
+};
